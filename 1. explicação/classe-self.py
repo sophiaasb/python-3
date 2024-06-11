9a836f4a52f37ee443cf31ac724aaca3dd2a24da
Sophia Angelina <161032086+sophiaasb@users.noreply.github.com>
2024-06-11 12:42:32 +0000

    main

--- 1. explicação/classe-self.py
+++ 1. explicação/classe-self.py
@@ -5,18 +5,24 @@ os.system("cls || clear")
 QUANTIDADE_ALUNOS = 2
 
 class Aluno:
-    def __init__(self, nome, idade):
+    def __init__(self, nome, idade, peso, altura):
         self.nome = nome
         self.idade = idade
+        self.peso = peso
+        self.altura = altura
 
 alunos = []
 
 for i in range(QUANTIDADE_ALUNOS):
     nome_aluno = input("Digite seu nome: ")
     idade_aluno = int(input("Digite sua idade: "))
-    aluno = Aluno(nome = nome_aluno, idade = idade_aluno)
+    peso_aluno = float(input("Digite seu peso: "))
+    altura_aluno = float(input("Digite sua altura: "))
+    aluno = Aluno(nome = nome_aluno, idade = idade_aluno, peso = peso_aluno, altura = altura_aluno)
     alunos.append(aluno)
 
 for dados_aluno in alunos:
     print(f"Nome: {dados_aluno.nome}")
     print(f"Idade: {dados_aluno.idade}")
+    print(f"Peso: {dados_aluno.peso}")
+    print(f"Altura: {dados_aluno.altura}")
